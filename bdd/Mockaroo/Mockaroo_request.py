f1edd8c39698fae746c113f08d222ae48c118141
chesbedoya <59845361+chesbedoya@users.noreply.github.com>
2022-01-13 15:13:10 -0500

    Update Mockaroo_request.py

--- bdd/Mockaroo/Mockaroo_request.py
+++ bdd/Mockaroo/Mockaroo_request.py
@@ -3,16 +3,17 @@ import requests
 import json
 import bdd.config.global_configuration as gc
 
+
 class Mockaroo_request:
 
-    def post_request(self,schema,num_rows_to_generate):
+    def post_request(self, schema, num_rows_to_generate):
         try:
-            url = gc.MOCKAROO_URL + '?key=' + gc.MOCKAROO_KEY + '&count' + str(num_rows_to_generate) + '&schema=' + schema
+            url = gc.MOCKAROO_URL + '?key=' + gc.MOCKAROO_KEY + '&count=' + str(num_rows_to_generate) + '&schema=' + schema
             response = requests.post(url)
             json_response = json.loads(response.content)
             return json_response
         except Exception as e:
-            print('error al conectar con el API de Mockaroo' + str(e))
+            print("Error al conectar con el api de Mockaroo " + str(e))
 
     def get_information_passenger_data(self):
         return self.post_request('Schema000001', '1')
