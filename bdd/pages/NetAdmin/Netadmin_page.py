cf0390708b79785d64ed90548be37cfab847e4a7
Jorge Ramses Bedoya Suarez <59845361+chesbedoya@users.noreply.github.com>
2022-03-17 10:13:25 -0500

    Update Netadmin_page.py

--- bdd/pages/NetAdmin/Netadmin_page.py
+++ bdd/pages/NetAdmin/Netadmin_page.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import TimeoutException
 from bdd.pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
