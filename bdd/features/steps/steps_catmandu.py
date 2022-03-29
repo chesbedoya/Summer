63767af751e55f3d6c833096a5ad2d327cc77e31
Jorge Ramses Bedoya Suarez <59845361+chesbedoya@users.noreply.github.com>
2022-03-29 14:38:23 -0500

    uodate

--- bdd/features/steps/steps_catmandu.py
+++ bdd/features/steps/steps_catmandu.py
@@ -1,5 +1,8 @@
 import time
 from behave import *
+
+from bdd.pages.catmandu.Car_Result_Page import car_result_page
+from bdd.pages.catmandu.Extras_Result_Page import extras_result_page
 from bdd.pages.catmandu.Hotel_Result_Page import hotel_result_page
 from bdd.pages.catmandu.Air_Result_Page import air_result_page
 from bdd.pages.catmandu.Passenger_page import Passenger_page
@@ -8,9 +11,7 @@ from bdd.pages.catmandu.Air_Details import air_details
 
 @Given("Hacer búsqueda de hoteles en catmandu para ocupación {occupancy:w} para destino iata {destination:w}"
        " con fecha de checkin en {check_future_days:d} días y checkout en {check_out_future_days:d} días")
-
 def step_impl(context, occupancy, destination, check_future_days, check_out_future_days):
-
     page = hotel_result_page(context)
     page.search_hotel(city=destination, check_future_days=check_future_days, check_out_future_days=check_out_future_days
                       , occupancy=occupancy)
@@ -21,18 +22,30 @@ def step_impl(context, occupancy, destination, check_future_days, check_out_futu
 def step_impl(context):
     context.current_page.wait_results_hotel()
 
+<<<<<<< HEAD
+=======
+
+@Then("Quitar filtro de solo hoteles con imagenes")
+def step_impl(context):
+    context.current_page.delete_filter_hoteles()
+>>>>>>> victorino
+
 
 @Then("Seleccionar la primera opción de hotel")
 def step_impl(context):
     context.current_page.click_option_hotel()
 
-@Then("Seleccionar la opción {roomOption:d} del hotel {hotelOption:d} de la página de resultados de catmandú")
-def step_impl(context,hotelOption, roomOption):
 
+@Then("Seleccionar la opción {roomOption:d} del hotel {hotelOption:d} de la página de resultados de catmandú")
+def step_impl(context, hotelOption, roomOption):
     if hotelOption >= 1:
-        hotelOption = hotelOption-1
+        hotelOption = hotelOption - 1
     if roomOption >= 1:
+<<<<<<< HEAD
         roomOption = roomOption-1
+=======
+        roomOption = roomOption - 1
+>>>>>>> victorino
 
     page = hotel_result_page(context)
     page.click_hotel_option_dinamic(hotelOption, roomOption)
@@ -43,6 +56,7 @@ def step_impl(context):
     context.currentPage = Passenger_page(context)
     context.currentPage.wait_for_passenger_page()
 
+
 @Then("Esperar la validación de precios en página de pasajeros de catmandú")
 def step_impl(context):
     context.currentPage.wait_until_price_validation()
@@ -51,26 +65,39 @@ def step_impl(context):
 def step_impl(context):
     context.currentPage.fill_passenger_information()
 
+<<<<<<< HEAD
+=======
+
+@Then("Validar que el precio en la pagina de hoteles sea el mismo precio que en pagina de pasajeros")
+def step_impl(context):
+    context.currentPage.obteined_passenger_price()
+    context.currentPage.validation_price_passenger()
+
+
+>>>>>>> victorino
 @Then("Click botón continuar en página de pasajeros")
 def step_impl(context):
     context.currentPage.click_button_continue()
 
 
-@Given('Hacer búsqueda de aéreo en catmandu {trip_type:w} con la aerolinea {airline_code:w} para ocupación {passenger_combination:w} saliendo desde {city_from:w} con destino {city_to:w} con fecha de salida en {departure_future_days:d} días')
+@Given(
+    'Hacer búsqueda de aéreo en catmandu {trip_type:w} con la aerolinea {airline_code:w} para ocupación {passenger_combination:w} saliendo desde {city_from:w} con destino {city_to:w} con fecha de salida en {departure_future_days:d} días')
 def step_impl(context, trip_type, airline_code, passenger_combination, city_from, city_to, departure_future_days):
-
     page = air_result_page(context)
     page.search_air(trip_type=trip_type, airline_code=airline_code, passenger_combination=passenger_combination,
                     city_from=city_from, city_to=city_to, departure_future_days=departure_future_days)
     context.current_page = page
 
+
 @Then("Esperar que la página de resultados traiga aéreos en catmandú")
 def step_impl(context):
     context.current_page.wait_results_air()
 
+
 @Then("Seleccionar la primera opción con upsell")
 def step_impl(context):
     context.current_page.click_with_upsell_enabled()
+
 
 @Then("Esperar que la página de upsell se visualice en catmandu")
 def step_impl(context):
@@ -78,14 +105,123 @@ def step_impl(context):
     page.wait_results_upsell()
     context.currentPage = page
 
+
 @Then('Seleccionar la opción "{upsell_option}" del upsell')
 def step_impl(context, upsell_option):
     upsell_option = upsell_option.lower()
     if upsell_option == "mas costosa":
         context.currentPage.choose_max_price_upsell_option()
 
-@Then('Click en botón seguir')
+
+@Then("Click en botón seguir")
 def step_impl(context):
     page = air_details(context)
     page.click_button_upsell_continue()
     context.currentPage = page
+
+
+@given("Hacer búsqueda de extras en {extra_city_from:w} con fecha desde en {extra_start_future_days:d}"
+       " días y fecha hasta en {extra_end_future_days:d} días y ocupacion {occupancy:w}")
+def step_impl(context, extra_city_from, extra_start_future_days, extra_end_future_days, occupancy):
+    page = extras_result_page(context)
+    page.search_extra(extra_city_from=extra_city_from, extra_start_future_days=extra_start_future_days,
+                      extra_end_future_days=extra_end_future_days, occupancy=occupancy)
+    context.current_page = page
+
+
+@Then('Esperar que la página de resultados traiga extras en catmandú')
+def step_impl(context):
+    context.current_page.wait_extra_results()
+
+
+@Then('Seleccionar la opción de extra {option:d} de la página de resultados de extras de catmandú')
+def step_impl(context, option):
+    context.current_page.select_option_extra(option)
+
+
+@Then('Esperar que la pagina de productos de catmandu cargue en su totalidad')
+def step_impl(context):
+    context.current_page.wait_product_view()
+    context.current_page.obteined_extras_price()
+
+
+@Then('Ingresar cantidad mínima de pasajeros en la pagina de productos de extras de catmandu')
+def step_impl(context):
+    context.current_page.load_quantity_person()
+
+
+@Then("Hacer click en el boton comprar ahora de la pagina de productos de extras de catmandu")
+def step_impl(context):
+    context.current_page.click_button_purchase()
+
+
+@then("Esperar a que se muestre la pagina de pasajeros en catmandú")
+def step_impl(context):
+    context.currentPage = Passenger_page(context)
+    context.currentPage.wait_until_price_validation()
+    context.currentPage.wait_for_passenger_page()
+
+
+@then('Llenar formulario de pasajero de extras en la página de pasajeros de catmandú')
+def step_impl(context):
+    context.currentPage.obteined_passenger_price()
+    context.currentPage.validation_extra_price_passenger()
+    context.currentPage.fill_passenger_information()
+
+
+@Then("Validar que el precio de pagina de resultados sea igual al precio de pagina de pasajeros en el flujo de extras")
+def step_impl(context):
+    context.currentPage.obteined_passenger_price()
+    context.currentPage.validation_extra_price_passenger()
+
+
+@Then("Dar click en el botón continuar en la página de pasajeros de catmandú")
+def step_impl(context):
+    context.currentPage.click_button_continue()
+
+
+@given("Hacer búsqueda de auto en {car_city_from:w} con fecha desde en {car_pickup_future_days:d}"
+       " días y fecha hasta en {car_delivery_future_days:d} días")
+def step_impl(context, car_city_from, car_pickup_future_days, car_delivery_future_days):
+    page = car_result_page(context)
+    page.search_car(car_city_from=car_city_from, car_pickup_future_days=car_pickup_future_days,
+                    car_delivery_future_days=car_delivery_future_days)
+    context.current_page = page
+
+
+@Then("Esperar que la página de resultados traiga autos en catmandú")
+def step_impl(context):
+    context.current_page.wait_car_results()
+
+
+@Then('Seleccionar la opción de auto {option:d} de la página de resultados de catmandú')
+def step_impl(context, option):
+    context.current_page.select_option_car(option)
+
+@Then('Esperar a que se muestre la página de detalles de auto en catmandú')
+def step_impl(context):
+    context.current_page.wait_car_product_view()
+
+@Then('Hacer click en la opcion del radio button pagar ahora de la pagina de detalle en catmandu')
+def step_impl(context):
+    context.current_page.select_radio_pay()
+    context.current_page.obteined_car_price()
+
+
+@Then('Continuar de la pagina de detalles de auto de catmandu a la pagina de pago')
+def step_impl(context):
+    context.current_page.continue_button_click()
+
+@then("Llenar formulario de conductor en la página de pasajeros de catmandú")
+def step_impl(context):
+    context.currentPage.obteined_passenger_car_price()
+    context.currentPage.fill_passenger_information()
+
+
+
+@Then("Validar que el precio de pagina de resultados sea igual al precio de pagina de pasajeros en el flujo de car")
+def step_impl(context):
+    context.currentPage.validation_car_price_passenger()
+
+
+
--- bdd/pages/catmandu/Passenger_page.py
+++ bdd/pages/catmandu/Passenger_page.py
@@ -1,3 +1,4 @@
+import re
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -20,6 +21,22 @@ class Passenger_page(BasePage):
     PASSENGER_EMAIL = (By.ID, "ContactEmail")
     PASSENGER_PHONE = (By.ID, "ContactPhone")
     PASSENGER_CHECKBOX = "chkTermsAndConditions"
+    EXTRA_TRAVELER_TITLE = "ExtraReservationItems_0__Travelers_0__Title"
+    EXTRA_TRAVELER_NAME = "ExtraReservationItems_0__Travelers_0__FirstName"
+    EXTRA_TRAVELER_LAST_NAME = "ExtraReservationItems_0__Travelers_0__LastName"
+    EXTRA_TRAVELER_DOCUMENT_TYPE = "ExtraReservationItems_0__Travelers_0__DocumentType"
+    EXTRA_TRAVELER_DOCUMENT = "ExtraReservationItems_0__Travelers_0__DucumentNumber"
+    EXTRA_TRAVELER_DOB_D = "ExtraReservationItems[0].Travelers[0].DOB_d"
+    EXTRA_TRAVELER_DOB_M = "ExtraReservationItems[0].Travelers[0].DOB_m"
+    EXTRA_TRAVELER_DOB_Y = "ExtraReservationItems[0].Travelers[0].DOB_y"
+    DRIVER_TITLE = "Driver_Title"
+    DRIVER_NAME = "Driver_FirstName"
+    DRIVER_LAST_NAME = "Driver_LastName"
+    DRIVER_DOCUMENT_TYPE = "Driver_DocumentType"
+    DRIVER_DOCUMENT = "Driver_DucumentNumber"
+    DRIVER_DOB_D = "Driver.DOB_d"
+    DRIVER_DOB_M = "Driver.DOB_m"
+    DRIVER_DOB_Y = "Driver.DOB_y"
 
     def __init__(self, context):
         BasePage.__init__(self, context)
@@ -35,6 +52,31 @@ class Passenger_page(BasePage):
                                                        ((By.ID, "message")))
             if not element.is_displayed():
                 return
+<<<<<<< HEAD
+=======
+    def obteined_passenger_price(self):
+        passenger_price = self.context.browser.find_elements_by_xpath("//div[@id='headerTotal']//span[@class='currencyText']")
+        price_result_passenger = passenger_price[0].text
+        price_result_passenger_replace = price_result_passenger.replace("$ ", "").replace(".", "")
+        price_passenger_float_results = float(price_result_passenger_replace)
+        self.context.passenger_price_validation = price_passenger_float_results
+
+    def obteined_passenger_car_price(self):
+        passenger_price = self.context.browser.find_elements_by_xpath("//div[@class='small-8 column text-right bold']//span[@class='nts-totalizer nts-big-total']//span[@class='currencyText']")
+        price_result_passenger = passenger_price[0].text
+        price_result_passenger_replace = price_result_passenger.replace("$ ", "").replace(".", "")
+        price_passenger_float_results = float(price_result_passenger_replace)
+        self.context.passenger_price_validation = price_passenger_float_results
+
+    def validation_price_passenger(self):
+        assert self.context.hotel_price_options == self.context.passenger_price_validation
+>>>>>>> victorino
+
+    def validation_extra_price_passenger(self):
+        assert self.context.extras_price_options == self.context.passenger_price_validation
+
+    def validation_car_price_passenger(self):
+        assert self.context.car_price_options == self.context.passenger_price_validation
 
     def fill_passenger_information(self):
         occupancy = self.get_occupancy_model()
@@ -45,11 +87,30 @@ class Passenger_page(BasePage):
             self.fill_phone(data['Phone'])
             self.click_checkbox()
 
-        if occupancy == '1r2a' or occupancy == '2adt':
+        elif occupancy == '1r2a' or occupancy == '2adt':
             self.fill_passenger1r2a()
             self.fill_email(data['Email'])
             self.fill_phone(data['Phone'])
             self.click_checkbox()
+
+        elif occupancy == '1r2a1c':
+            self.fill_passenger1r2a1c()
+            self.fill_email(data['Email'])
+            self.fill_phone(data['Phone'])
+            self.click_checkbox()
+
+        elif occupancy == '1re':
+            self.fill_traveler()
+            self.fill_email(data['Email'])
+            self.fill_phone(data['Phone'])
+            self.click_checkbox()
+
+        elif occupancy == '1drv':
+            self.fill_driver()
+            self.fill_email(data['Email'])
+            self.fill_phone(data['Phone'])
+            self.click_checkbox()
+
 
     def fill_passenger1r1a(self):
         self.fill_form_passenger('primary_adult', 0)
@@ -57,6 +118,17 @@ class Passenger_page(BasePage):
     def fill_passenger1r2a(self):
         self.fill_form_passenger('primary_adult', 0)
         self.fill_form_passenger('secondary_adult', 1)
+
+    def fill_passenger1r2a1c(self):
+        self.fill_form_passenger('primary_adult', 0)
+        self.fill_form_passenger('secondary_adult', 1)
+        self.fill_form_passenger('primary_children', 2)
+
+    def fill_traveler(self):
+        self.fill_form_passenger('traveler', 0)
+
+    def fill_driver(self):
+        self.fill_form_passenger('driver', 0)
 
 
     def fill_form_passenger(self, type_passenger, passenger_number):
@@ -73,6 +145,41 @@ class Passenger_page(BasePage):
             self.fill_birthday_month(date_object.month, passenger_number)
             self.fill_birthday_year(date_object.year, passenger_number)
 
+        elif type_passenger == 'primary_children':
+            self.fill_title(data['Title'], passenger_number)
+            self.fill_name(data['FirstName'], passenger_number)
+            self.fill_last_name(data['LastName'], passenger_number)
+            self.fill_document_type(['BillingDocumentTypeNamePlaceToPay'], passenger_number)
+            self.fill_document_number(self.mockaroo.get_document_passenger(), passenger_number)
+            date_object = datetime.datetime.strptime(data['ChildAge8'], date_time_format)
+            self.fill_birthday(date_object.day, passenger_number)
+            self.fill_birthday_month(date_object.month, passenger_number)
+            self.fill_birthday_year(date_object.year, passenger_number)
+
+
+        elif type_passenger == 'traveler':
+            self.fill_extra_traveler_title(data['Title'])
+            self.fill_extra_traveler_name(data['FirstName'])
+            self.fill_extra_traveler_last_name(data['LastName'])
+            self.fill_extra_traveler_document_type(data['BillingDocumentTypeNamePlaceToPay'])
+            self.fill_extra_traveler_document_number(self.mockaroo.get_document_passenger())
+            date_object = datetime.datetime.strptime(data['AdultAge'], date_time_format)
+            self.fill_extra_traveler_birth_day(date_object.day)
+            self.fill_extra_traveler_birth_month(date_object.month)
+            self.fill_extra_traveler_birth_year(date_object.year)
+
+        elif type_passenger == 'driver':
+            self.fill_driver_title(data['Title'])
+            self.fill_driver_name(data['FirstName'])
+            self.fill_driver_last_name(data['LastName'])
+            self.fill_driver_document_number(data['BillingDocumentTypeNamePlaceToPay'])
+            self.fill_driver_document_type(self.mockaroo.get_document_passenger())
+            date_object = datetime.datetime.strptime(data['AdultAge'], date_time_format)
+            self.fill_driver_birth_day(date_object.day)
+            self.fill_driver_birth_month(date_object.month)
+            self.fill_driver_birth_year(date_object.year)
+
+
     def get_occupancy_model(self):
         search_json = self.context.browser.execute_script('return $searchData')
         if hasattr(self.context, 'current_product') and self.context.current_product != 'hotel':
@@ -87,23 +194,39 @@ class Passenger_page(BasePage):
                     if quantity_air_passenger == 1 and passenger_type == 0:
                         air_occupancy = '1adt'
                         return "{}".format(air_occupancy)
+
+            elif self.context.current_product == 'extra':
+                if self.context.ocupation_extra == '1re':
+                    self.context.ocupation = '1re'
+                return self.context.ocupation
+
+            elif self.context.current_product == 'car':
+                return "1drv"
+
         else:
             rooms = search_json['Rooms']
             adults_rooms_one = 0
+            children_rooms_one = 0
             if len(rooms) == 1:
                 guests = search_json['Rooms'][0]['Guests']
                 for i in guests:
                     if i['Type'] == 0:
                         adults_rooms_one = adults_rooms_one + 1
-            occupancy = self.make_occupancy(len(rooms), adults_rooms_one)
+                    elif i['Type'] == 1:
+                        children_rooms_one = children_rooms_one + 1
+
+            occupancy = self.make_occupancy(len(rooms), adults_rooms_one, children_rooms_one)
             return occupancy
 
-    def make_occupancy(self, rooms, adults_rooms_one):
+    def make_occupancy(self, rooms, adults_rooms_one, children_rooms_one):
         if rooms == 1:
             if adults_rooms_one == 1:
                 return '1r1a'
+            elif adults_rooms_one == 2 and children_rooms_one == 1:
+                return '1r2a1c'
             elif adults_rooms_one == 2:
                 return '1r2a'
+
 
     def fill_title(self, title, index):
         element = (By.ID, self.PASSENGER_TITLE.replace('{index}', str(index)))
@@ -168,3 +291,92 @@ class Passenger_page(BasePage):
         WebDriverWait(self.context.browser, 20).until(
             EC.element_to_be_clickable(
                 (By.ID, 'submitButtonId'))).click()
+
+
+    def fill_extra_traveler_title(self, title):
+        by = (By.ID, self.EXTRA_TRAVELER_TITLE)
+        WebDriverWait(self.context.browser, 20).until(
+            EC.visibility_of_element_located((By.XPATH, f"//*[@id='ExtraReservationItems_0__Travelers_0__Title']/option[text()='{title}']"))).click()
+
+    def fill_extra_traveler_name(self, name):
+        by = (By.ID, self.EXTRA_TRAVELER_NAME)
+        name = re.sub(r"[^\w.@-]", "", name)
+        #name = name.replace("'", "") if "'" in name else name
+        WebDriverWait(self.context.browser, 20).until(
+            EC.visibility_of_element_located(by)).send_keys(name)
+
+    def fill_extra_traveler_last_name(self, last_name):
+        by = (By.ID, self.EXTRA_TRAVELER_LAST_NAME)
+        last_name = re.sub(r"[^\w.@-]", "", last_name)
+        #last_name = last_name.replace("'", "") if "'" in last_name else last_name
+        WebDriverWait(self.context.browser, 20).until(
+            EC.visibility_of_element_located(by)).send_keys(last_name)
+
+    def fill_extra_traveler_document_type(self, type_document):
+        by = (By.ID, self.EXTRA_TRAVELER_DOCUMENT_TYPE)
+        WebDriverWait(self.context.browser, 30).until(
+            EC.visibility_of_element_located(by)).send_keys(type_document)
+
+    def fill_extra_traveler_document_number(self, document):
+        by = (By.ID, self.EXTRA_TRAVELER_DOCUMENT)
+        WebDriverWait(self.context.browser, 30).until(
+            EC.visibility_of_element_located(by)).send_keys(document)
+
+    def fill_extra_traveler_birth_day(self, day):
+        by = (By.ID, self.EXTRA_TRAVELER_DOB_D)
+        WebDriverWait(self.context.browser, 20).until(
+            EC.visibility_of_element_located((By.XPATH, f"//*[@id='ExtraReservationItems[0].Travelers[0].DOB_d']/option[text()='{day}']"))).click()
+
+    def fill_extra_traveler_birth_month(self, month):
+        by = (By.ID, self.EXTRA_TRAVELER_DOB_M)
+        WebDriverWait(self.context.browser, 20).until(
+            EC.visibility_of_element_located((By.XPATH, f"//*[@id='ExtraReservationItems[0].Travelers[0].DOB_m']/option[text()='{month}']"))).click()
+
+    def fill_extra_traveler_birth_year(self, year):
+        by = (By.ID, self.EXTRA_TRAVELER_DOB_Y)
+        WebDriverWait(self.context.browser, 20).until(
+            EC.visibility_of_element_located((By.XPATH, f"//*[@id='ExtraReservationItems[0].Travelers[0].DOB_y']/option[text()='{year}']"))).click()
+
+    def fill_driver_title(self, title):
+        by = (By.ID, self.DRIVER_TITLE)
+        WebDriverWait(self.context.browser, 20).until(
+            EC.visibility_of_element_located((By.XPATH, f"//*[@id='Driver_Title']/option[text()='{title}']"))).click()
+
+    def fill_driver_name(self, name):
+        by = (By.ID, self.DRIVER_NAME)
+        name = re.sub(r"[^\w.@-]", "", name)
+        #name = name.replace("'", "") if "'" in name else name
+        WebDriverWait(self.context.browser, 20).until(
+            EC.visibility_of_element_located(by)).send_keys(name)
+
+    def fill_driver_last_name(self, last_name):
+        by = (By.ID, self.DRIVER_LAST_NAME)
+        last_name = re.sub(r"[^\w.@-]", "", last_name)
+        #last_name = last_name.replace("'", "") if "'" in last_name else last_name
+        WebDriverWait(self.context.browser, 20).until(
+            EC.visibility_of_element_located(by)).send_keys(last_name)
+
+    def fill_driver_document_number(self, document):
+        by = (By.ID, self.DRIVER_DOCUMENT_TYPE)
+        WebDriverWait(self.context.browser, 20).until(
+            EC.visibility_of_element_located(by)).send_keys(document)
+
+    def fill_driver_document_type(self, type_document):
+        by = (By.ID, self.DRIVER_DOCUMENT)
+        WebDriverWait(self.context.browser, 20).until(
+            EC.visibility_of_element_located(by)).send_keys(type_document)
+
+    def fill_driver_birth_day(self, day):
+        by = (By.ID, self.DRIVER_DOB_D)
+        WebDriverWait(self.context.browser, 20).until(
+            EC.visibility_of_element_located((By.XPATH, f"//*[@id='Driver.DOB_d']/option[text()='{day}']"))).click()
+
+    def fill_driver_birth_month(self, month):
+        by = (By.ID, self.DRIVER_DOB_M)
+        WebDriverWait(self.context.browser, 20).until(
+            EC.visibility_of_element_located((By.XPATH, f"//*[@id='Driver.DOB_m']/option[text()='{month}']"))).click()
+
+    def fill_driver_birth_year(self, year):
+        by = (By.ID, self.DRIVER_DOB_Y)
+        WebDriverWait(self.context.browser, 20).until(
+            EC.visibility_of_element_located((By.XPATH, f"//*[@id='Driver.DOB_y']/option[text()='{year}']"))).click()
